9ac18b0b04c83f54b0f566b9ea0a64f90e3a3adc
Matt Rudder <matt@mattrudder.com>
2012-04-01 22:05:21 -0700

    Fixing binding for vec3 and float.

--- demo/sandbox/sandbox.fs
+++ demo/sandbox/sandbox.fs
@@ -2,6 +2,9 @@
 precision highp float;
 #endif
 
+uniform float alpha;
+uniform vec3 color; 
+
 void main(void) {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = vec4(0, alpha, 0, 1.0);
 }	
--- src/Graphics/Material.js
+++ src/Graphics/Material.js
@@ -24,13 +24,83 @@ Tatsu.Material = function(ctx, options) {
     this.bindUniform = function(name, value) {
         var uniformType = this.uniformTypes[name];
 
-        switch (uniformType) {
-            case 'mat4':
-            {
-                _gl.uniformMatrix4fv(this.uniformLocations[name], _gl.FALSE, value);
+        function checkSize(type, expected, actual) {
+            if (expected !== actual) {
+                console.warn(type + ' expects array with a length of ' + expected + '; got ' + actual + '.');
             }
-            break;
-            // TODO: Support texture types.
+        }
+
+        if (uniformType) {
+            if (this.uniformLocations[name] === undefined) {
+                if (this.uniformTypes[name]) {
+                    this.uniformLocations[name] = _gl.getUniformLocation(this.program, name);
+                }
+                else {
+                    console.warn('uniform "' + name + '" is undefined!');
+                    return;
+                }
+            }
+
+            switch (uniformType) {
+                case 'float':
+                    if (typeof value === 'number') {
+                        _gl.uniform1f(this.uniformLocations[name], value);
+                        
+                    }
+                    else {
+                        checkSize(uniformType, 1, value.length);
+                        _gl.uniform1fv(this.uniformLocations[name], value);
+                    }
+                break;
+                case 'int':
+                    if (typeof value === 'number') {
+                        _gl.uniform1i(this.uniformLocations[name], value);
+                        
+                    }
+                    else {
+                        checkSize(uniformType, 1, value.length);
+                        _gl.uniform1iv(this.uniformLocations[name], value);
+                    }
+                break;
+                case 'bool':
+                    if (typeof value === 'boolean') {
+                        _gl.uniform1b(this.uniformLocations[name], value);
+                        
+                    }
+                    else {
+                        checkSize(uniformType, 1, value.length);
+                        _gl.uniform1bv(this.uniformLocations[name], value);
+                    }
+                break;
+                case 'mat2':
+                    checkSize(uniformType, 4, value.length);
+                    _gl.uniformMatrix2fv(this.uniformLocations[name], _gl.FALSE, value);
+                break;
+                case 'mat3':
+                    checkSize(uniformType, 9, value.length);
+                    _gl.uniformMatrix3fv(this.uniformLocations[name], _gl.FALSE, value);
+                break;
+                case 'mat4':
+                    checkSize(uniformType, 16, value.length);
+                    _gl.uniformMatrix4fv(this.uniformLocations[name], _gl.FALSE, value);
+                break;
+                case 'vec2':
+                    checkSize(uniformType, 2, value.length);
+                    _gl.uniform2fv(this.uniformLocations[name], value);
+                break;
+                case 'vec3':
+                    checkSize(uniformType, 3, value.length);
+                    _gl.uniform3fv(this.uniformLocations[name], value);
+                break;
+                case 'vec4':
+                    checkSize(uniformType, 4, value.length);
+                    _gl.uniform4fv(this.uniformLocations[name], value);
+                break;
+                default:
+                // TODO: Support texture types.
+                console.error('uniform type "' + uniformType + '" not supported!');
+                break;
+            }
         }
     }
 
