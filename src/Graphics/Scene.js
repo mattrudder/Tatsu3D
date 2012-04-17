ac36ddab48e5ec37a1a2a67e5df50e21d04e6a68
Matt Rudder <matt@mattrudder.com>
2012-04-16 22:58:49 -0700

    Skeleton files for Scene and SpriteBatch.

---
+++ src/Graphics/Scene.js
@@ -0,0 +1,11 @@
+
+Tatsu.Scene = function(ctx, options) {
+    // private members
+    var _gl = ctx.gl(),
+        _self = this,
+        _options = options || {};
+
+    this.draw = function () {
+        
+    };
+};
